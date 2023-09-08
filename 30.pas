95f0538b4846c7bbee05909c399fbb999bf03bc9
Laptya <144488450+Laptya@users.noreply.github.com>
2023-09-09 01:09:13 +0300

    Update 30.pas

--- 30.pas
+++ 30.pas
@@ -1,8 +1,10 @@
 var x,a,b,i:integer;
 begin
-for x:=1 to 100 do begin
+writeln('Введите число в диапазоне от 1 до 100 ');
+repeat readln(x) until (x>=1) or (x<=100);
 a:=x div 10;
 b:=x mod 10;
+write('Число ',x,' в римской системе счисления = ');
 if a=10 then write('C') else
 if a<4 then for i:=1 to a do write('X') else
 if a=4 then write('XL') else if a<=8 then begin write('L'); for i:=1 to a mod 5 do write('X'); end 
@@ -11,7 +13,4 @@ else if a=9 then write('XC');
 if b=9 then write('IX') else if b =4 then write('IV')
        else if b div 5 = 1 
          then begin write('V'); for i:=1 to b mod 5 do write('I'); end else for i:=1 to b do write('I');
-         writeln('     ',x)
-         end;
 end.
-
