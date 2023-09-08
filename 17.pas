8690d30fbb5504e8a8bd1f3f74a022a2145d2641
Laptya <144488450+Laptya@users.noreply.github.com>
2023-09-09 01:03:16 +0300

    Add files via upload

---
+++ 17.pas
@@ -0,0 +1,12 @@
+var a,b,c,d,r:integer;
+x1,x2:real;
+begin
+  readln(a);
+  readln(b);
+  readln(c);
+  d:=sqr(b)-4*a*c;
+  if d<0 then writeln('Нет корней');
+  if d=0 then begin x1:=-b/(2*a); write('Корень равен ',x1:0:3) end;
+  if d>0 then begin x1:=(-b-sqrt(d))/(2*a); x2:=(-b+sqrt(d))/(2*a); writeln('Корни равны ',x1:0:3,' ',x2:0:3) end
+end.
+
---
+++ 19.pas
@@ -0,0 +1,10 @@
+var x,a,b,c,d:integer;
+begin
+readln(x);
+a:=x div 1000;
+b:=x div 100 mod 10;
+c:=x mod 100 div 10;
+d:=x mod 10;
+if (a=d) and (b=c) then write('Палиндром') else write('Не палиндром');
+end.
+
